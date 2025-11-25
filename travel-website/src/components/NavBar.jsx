e2ef4d33e5d9532d8864a6f64dc29d1a23df18dc
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2025-11-25 10:26:20 +0530

    feat: update NavBar component structure; change logo to link and wrap social icons in anchor tags

--- travel-website/src/components/NavBar.jsx
+++ travel-website/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import '../styles/NavBar.css';
 function NavBar() {
   return (
     <nav className="hero-nav">
-      <div className="logo">Travel Lanka</div>
+      <a className="logo" href="/">Travel Lanka</a>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
@@ -13,9 +13,9 @@ function NavBar() {
         <li><Link to="/contact">Contact</Link></li>
       </ul>
       <div className="social-icons">
-        <i className="fab fa-facebook-f"></i>
-        <i className="fab fa-instagram"></i>
-        <i className="fab fa-linkedin-in"></i>
+        <a href="https://facebook.com" ><i className="fab fa-facebook-f"></i></a>
+        <a href="https://instagram.com" ><i className="fab fa-instagram"></i></a>
+        <a href="https://linkedin.com" ><i className="fab fa-linkedin-in"></i></a>
       </div>
     </nav>
   );
